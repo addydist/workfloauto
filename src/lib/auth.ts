c2e7f95c7c4d5387b6ab606f2b5d78615c30f94d
Aditya pal <90037838+addydist@users.noreply.github.com>
2026-01-26 13:21:33 +0530

    payments by polar

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,14 +1,35 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { checkout, polar, portal } from "@polar-sh/better-auth";
 // If your Prisma file is located elsewhere, you can change the path
 import { PrismaClient } from "@/generated/prisma/client";
 import prisma from "./db";
+import { polarClient } from "./polar";
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql",
-    }),
-    emailAndPassword: {
-        enabled: true,
-        autoSignIn: true,
-    },
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: true,
+  },
+  plugins: [
+    polar({
+        client: polarClient,
+        createCustomerOnSignUp: true,
+        use: [
+            checkout({
+                products: [
+                    {
+                        productId: "5445093a-94ca-486b-8f48-9326986d8165",
+                        slug: "Nodeflo-pro" // Custom slug for easy reference in Checkout URL, e.g. /checkout/Nodeflo-pro
+                    }
+                ],
+                successUrl: process.env.POLAR_SUCCESS_URL,
+                authenticatedUsersOnly: true
+            }),
+            portal(),
+        ],
+    })
+]
 });
--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,13 +1,14 @@
-import { auth } from '@/lib/auth';
-import { initTRPC, TRPCError } from '@trpc/server';
-import { se } from 'date-fns/locale';
-import { headers } from 'next/headers';
-import { cache } from 'react';
+import { auth } from "@/lib/auth";
+import { polarClient } from "@/lib/polar";
+import { initTRPC, TRPCError } from "@trpc/server";
+import { se } from "date-fns/locale";
+import { headers } from "next/headers";
+import { cache } from "react";
 export const createTRPCContext = cache(async () => {
   /**
    * @see: https://trpc.io/docs/server/context
    */
-  return { userId: 'user_123' };
+  return { userId: "user_123" };
 });
 // Avoid exporting the entire t-object
 // since it's not very descriptive.
@@ -25,10 +26,27 @@ export const createCallerFactory = t.createCallerFactory;
 export const baseProcedure = t.procedure;
 export const protectedProcedure = baseProcedure.use(async ({ ctx, next }) => {
   const session = await auth.api.getSession({
-    headers:await headers(),
+    headers: await headers(),
   });
-  if(!session){
-    throw new TRPCError({code:'UNAUTHORIZED',message:'Unauthorized'});  
+  if (!session) {
+    throw new TRPCError({ code: "UNAUTHORIZED", message: "Unauthorized" });
   }
-  return next({ctx:{...ctx,auth:session}});
+  return next({ ctx: { ...ctx, auth: session } });
 });
+export const premiumProcedure = protectedProcedure.use(
+  async ({ ctx, next }) => {
+    const customer = await polarClient.customers.getStateExternal({
+      externalId: ctx.auth.user.id,
+    });
+    if (
+      !customer.activeSubscriptions ||
+      customer.activeSubscriptions.length === 0
+    ) {
+      throw new TRPCError({
+        code: "FORBIDDEN",
+        message: "Premium membership required",
+      });
+    }
+    return next({ ctx: { ...ctx, customer } });
+  }
+);
