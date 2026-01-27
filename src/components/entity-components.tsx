1361739f4bd0308222d6c2ce661e1bf9ddf53a71
Aditya pal <90037838+addydist@users.noreply.github.com>
2026-01-27 17:58:48 +0530

    workflow-pagination

--- src/components/entity-components.tsx
+++ src/components/entity-components.tsx
@@ -1,7 +1,8 @@
-import { PlusIcon } from "lucide-react";
+import { PlusIcon, Search, SearchIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import Link from "next/link";
 import { se } from "date-fns/locale";
+import { Input } from "./ui/input";
 
 type EntityHeaderProps = {
   title: string;
@@ -26,7 +27,7 @@ export const EntityHeader = ({
   return (
     <div className="flex flex-row items-center justify-between gap-x-4">
       <div className="flex flex-col">
-        <h1 className="text-lg md:text-xl fonst font-semibold">{title}</h1>
+        <h1 className="text-lg md:text-xl font font-semibold">{title}</h1>
         {description && (
           <p className="text-xs md:text-sm text-muted-foreground">
             {description}
@@ -65,13 +66,80 @@ export const EntityContaniner = ({
   pagination,
 }: EntityContainerProps) => {
   return (
-    <div className="p-4 md:px-10 md:py-6 h-full">
-      <div className="mx-auto max-w-screen w-full flex flex-col gap-y-8 h-full">
-        {header}
-        <div className="flex flex-col gap-y-full">
-          {search}
-          {children}
-        </div>
+    <div className="h-full p-4 md:px-10 md:py-6">
+    <div className="mx-auto w-full max-w-screen flex h-full flex-col gap-6">
+      {header}
+
+      <div className="flex flex-1 flex-col gap-4">
+        {search}
+        {children}
+      </div>
+
+      {pagination}
+    </div>
+  </div>
+
+  );
+};
+
+interface EntitySearchProps {
+  value: string;
+  onChange: (value: string) => void;
+  placeholder: string;
+}
+export const EntitySearch = ({
+  value,
+  onChange,
+  placeholder = "Search...",
+}: EntitySearchProps) => {
+  return (
+    <div className="relative ml-auto">
+      <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
+      <Input
+        className="max-w-[200px] bg-background shadow-none border-border pl-8"
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
+    </div>
+  );
+};
+
+interface EntityPaginationProps {
+  page: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+  disabled?: boolean;
+}
+
+export const EntityPagination = ({
+  page,
+  totalPages,
+  onPageChange,
+  disabled,
+}: EntityPaginationProps) => {
+  return (
+    <div className="flex items-center justify-between gap-x-2 w-full">
+      <div className="flex-1 text-sm text-muted-foreground">
+        Page {page} of {totalPages || 1}
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-2">
+        <Button
+          disabled={page === 1 || disabled}
+          variant="outline"
+          size="sm"
+          onClick={() => onPageChange(Math.max(1, page - 1))}
+        >
+          Previous
+        </Button>
+        <Button
+          disabled={page === totalPages || disabled || totalPages === 0}
+          variant="outline"
+          size="sm"
+          onClick={() => onPageChange(Math.min(totalPages, page + 1))}
+        >
+          Next
+        </Button>
       </div>
     </div>
   );
