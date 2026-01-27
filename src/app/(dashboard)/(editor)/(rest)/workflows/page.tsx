1d243be96246e18f4effec664ae480034e3a7a90
Aditya pal <90037838+addydist@users.noreply.github.com>
2026-01-27 23:44:45 +0530

    workflow ui

--- src/app/(dashboard)/(editor)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(editor)/(rest)/workflows/page.tsx
@@ -3,7 +3,7 @@ import { requireAuth } from "@/lib/auth-utils";
 import { HydrateClient } from "@/trpc/server";
 import { ErrorBoundary } from "react-error-boundary";
 import { Suspense } from "react";
-import { WorkflowsContainer, WorkflowsList } from "@/features/workflows/components/workflows";
+import { WorkflowsContainer, WorkflowsError, WorkflowsList, WorkflowsLoading } from "@/features/workflows/components/workflows";
 import type { SearchParams } from "nuqs/server";
 import { workflowParamsLoader } from "@/features/workflows/server/params-loader";
 
@@ -17,8 +17,8 @@ const Page=async({searchParams}:Props) => {
     return (
       <WorkflowsContainer>
      <HydrateClient>
-        <ErrorBoundary fallback={<p>Error</p>}>
-          <Suspense fallback={<div>Loading workflows...</div>}>
+        <ErrorBoundary fallback={<WorkflowsError/>}>
+          <Suspense fallback={<WorkflowsLoading/>}>
             <WorkflowsList />
           </Suspense>
           </ErrorBoundary>
--- src/features/workflows/components/workflows.tsx
+++ src/features/workflows/components/workflows.tsx
@@ -1,15 +1,24 @@
 "use client";
-import { useCreateWorkflow, useSuspenseWorkflows } from "../hooks/use-workflow";
+import { useCreateWorkflow, useRemoveWorkflow, useSuspenseWorkflows } from "../hooks/use-workflow";
 import {
+  EmptyView,
   EntityContaniner,
   EntityHeader,
+  EntityItem,
+  EntityList,
   EntityPagination,
   EntitySearch,
+  ErrorView,
+  LoadingView,
 } from "@/components/entity-components";
 import { useUpgradeModal } from "@/hooks/use-upgrade-modal";
 import { useRouter } from "next/navigation";
 import { useWorkflowParams } from "../hooks/use-workflow-params";
 import { useEntitySearch } from "@/hooks/use-entity-search";
+import Error from "next/error";
+import { Workflow } from "@/generated/prisma";
+import { WorkflowIcon } from "lucide-react";
+import {formatDistanceToNow} from "date-fns"
 
 export const WorkFlowSearch = () => {
   const [params, setParams] = useWorkflowParams();
@@ -25,12 +34,50 @@ export const WorkFlowSearch = () => {
     />
   );
 };
+export const WorkflowsLoading = () => {
+  return <LoadingView entity="workflows" message="Loading workflows" />;
+};
+
+export const WorkflowsError = () => {
+  return <ErrorView message="Error loading workflows" />;
+};
+export const WorkflowsEmpty = () => {
+  const createWorkflow = useCreateWorkflow();
+  const { handleError, modal } = useUpgradeModal();
+  const router = useRouter();
+  const handleCreate = () => {
+    createWorkflow.mutate(undefined, {
+      onError: (error) => {
+        handleError(error);
+      },
+      onSuccess: (data) => {
+        router.push(`/workflows/${data.id}`);
+      },
+    });
+  };
+  return (
+    <>
+      {modal}
+      <EmptyView
+        onNew={handleCreate}
+        message="You haven't created any workflows yet.Get started by creating your first workflow"
+      />
+    </>
+  );
+};
+
 export const WorkflowsList = () => {
   const workflows = useSuspenseWorkflows();
+  // if(workflows?.data?.items?.length===0){
+  //   return <WorkflowsEmpty />;
+  // }
   return (
-    <div className="flex-1">
-      <p>{JSON.stringify(workflows.data, null, 2)}</p>
-    </div>
+    <EntityList
+      items={workflows?.data?.items}
+      getKey={(workflow) => workflow.id}
+      renderItem={(workflow) => <WorkflowItem data={workflow} />}
+      emptyView={<WorkflowsEmpty />}
+    />
   );
 };
 
@@ -90,5 +137,26 @@ export const WorkflowsContainer = ({
     >
       {children}
     </EntityContaniner>
+  );
+};
+
+export const WorkflowItem = ({ data }: { data: Workflow }) => {
+  const removeWorkflow=useRemoveWorkflow();
+  const handleRemove=()=>{
+    removeWorkflow.mutate({id:data.id});
+  }
+  return (
+    <EntityItem
+      href={`/workflows/${data.id}`}
+      title={data.name}
+      subtitle={<>Updated {formatDistanceToNow(data.updatedAt,{addSuffix:true})} &bull; Created {formatDistanceToNow(data.createdAt,{addSuffix:true})} </>}
+      image={
+        <div className="h-8 w-8 rounded-md bg-muted flex items-center justify-center">
+          <WorkflowIcon className="size-5 text-muted-foreground" />
+        </div>
+      }
+      onRemove={handleRemove}
+      isRemoving={removeWorkflow.isPending}
+    />
   );
 };
