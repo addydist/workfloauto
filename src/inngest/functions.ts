21cce2b3b16ee7e40c2ada7d6531bd9331a8d45f
Aditya pal <90037838+addydist@users.noreply.github.com>
2026-03-20 14:42:49 +0530

    Add execution management features including database schema, hooks, and UI components

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -3,7 +3,7 @@ import { inngest } from "./client";
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
 import { gemini, NonRetriableError } from "inngest";
 import { topologicalSort } from "./utils";
-import { NodeType } from "@/generated/prisma";
+import { ExecutionStatus, NodeType } from "@/generated/prisma";
 import { getExecutor } from "@/features/executions/lib/executor-registry";
 import { httpRequestChannel } from "./channels/http-request";
 import { manualTriggerChannel } from "./channels/manual-request";
@@ -16,9 +16,19 @@ import { discordChannel } from "./channels/discord";
 import { slackChannel } from "./channels/slack";
 const google = createGoogleGenerativeAI();
 export const executeWorkflow = inngest.createFunction(
-  { id: "execute-workflow",retries: 0 },
-  { event: "workflow/execute.workflow" ,
-    channels:[
+  { id: "execute-workflow", retries: 0,onFailure:async({event,step})=>{
+    return prisma.execution.update({
+      where:{inngestEventId:event.data.event.id},
+      data:{
+        status:ExecutionStatus.FAILED,
+        error:event.data.error.message,
+        errorStack:event.data.error.stack
+      }
+    })
+  } },
+  {
+    event: "workflow/execute.workflow",
+    channels: [
       httpRequestChannel(),
       manualTriggerChannel(),
       googleFromTriggerChannel(),
@@ -27,15 +37,23 @@ export const executeWorkflow = inngest.createFunction(
       openAiChannel(),
       anthropicChannel(),
       discordChannel(),
-      slackChannel()
-    ]
+      slackChannel(),
+    ],
   },
-  async ({ event, step,publish }) => {
+  async ({ event, step, publish }) => {
+    const inngestEventId = event.id;
     const workflowId = event.data.workflowId;
-    if (!workflowId) {
-      throw new NonRetriableError("No workflow ID provided");
+    if (!inngestEventId || !workflowId) {
+      throw new NonRetriableError("Event Id or workflow ID provided");
     }
-
+    await step.run("create-execution", async () => {
+      return prisma.execution.create({
+        data: {
+          workflowId,
+          inngestEventId,
+        },
+      });
+    });
     const sortedNodes = await step.run("prepare-workflow", async () => {
       const workflow = await prisma.workflow.findUniqueOrThrow({
         where: { id: workflowId },
@@ -46,15 +64,15 @@ export const executeWorkflow = inngest.createFunction(
       }
       return topologicalSort(workflow.nodes, workflow.connections);
     });
-    const userId=await step.run("find-user-id",async()=>{
+    const userId = await step.run("find-user-id", async () => {
       const workflow = await prisma.workflow.findUniqueOrThrow({
         where: { id: workflowId },
-        select:{
-          userId:true
-        }
+        select: {
+          userId: true,
+        },
       });
       return workflow.userId;
-    })
+    });
     let context = event.data.initialData || {};
     for (const node of sortedNodes) {
       const executor = getExecutor(node.type as NodeType);
@@ -64,9 +82,19 @@ export const executeWorkflow = inngest.createFunction(
         context,
         userId,
         step,
-        publish
+        publish,
       });
     }
+    await step.run("update-execution", async () => {
+      return prisma.execution.update({
+        where:{ inngestEventId,workflowId},
+        data: {
+           status:ExecutionStatus.SUCCESS,
+           completedAt:new Date(),
+           output:context
+        },
+      });
+    });
     return { workflowId, context };
-  }
+  },
 );
